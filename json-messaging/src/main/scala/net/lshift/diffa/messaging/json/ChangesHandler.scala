af1581e27bda89a00652eff6e811c8c0261645f9
Ben Hood <0x6e6562@gmail.com>
2010-12-02 15:23:19 +0000

    Wired in Changes JSON frontends to use new serialization structure [#150]

--- json-messaging/src/main/scala/net/lshift/diffa/messaging/json/ChangesHandler.scala
+++ json-messaging/src/main/scala/net/lshift/diffa/messaging/json/ChangesHandler.scala
@@ -18,9 +18,8 @@ package net.lshift.diffa.messaging.json
 
 
 import net.lshift.diffa.kernel.frontend.Changes
-import org.codehaus.jettison.json.JSONObject
-import net.lshift.diffa.kernel.events.{UpstreamChangeEvent, DownstreamChangeEvent, DownstreamCorrelatedChangeEvent}
-import collection.mutable.{ListBuffer, HashMap}
+import JSONEncodingUtils._
+import net.lshift.diffa.kernel.frontend.EventRegistry._
 
 /**
  * JSON protocol handler for change requests.
@@ -29,26 +28,6 @@ import collection.mutable.{ListBuffer, HashMap}
 class ChangesHandler(val frontend:Changes) extends AbstractJSONHandler {
 
   protected val endpoints = Map(
-    "changes" -> defineOnewayRpc((s:String) => s)(s => {
-      val jObj = new JSONObject(s)
-      val endpoint = jObj.getString("endpoint")
-      val id = jObj.getString("id")
-      // TODO This should be in the JSONEncodingUtil
-      val attributes = new ListBuffer[String]
-      val attributeArray = jObj.getJSONArray("attributes")
-      for (val i <- 0 to attributeArray.length - 1) {
-        attributes += attributeArray.getString(i)
-      }
-      val lastUpdated = JSONEncodingUtils.maybeParseableDate(jObj.optString("lastUpdated"))
-
-      val evt = jObj.getString("type") match {
-        case "upstream" => new UpstreamChangeEvent(endpoint, id, attributes, lastUpdated, jObj.optString("vsn", null))
-        case "downstream-same" => new DownstreamChangeEvent(endpoint, id, attributes, lastUpdated, jObj.optString("vsn", null))
-        case "downstream-correlated" =>
-          new DownstreamCorrelatedChangeEvent(endpoint, id, attributes, lastUpdated, jObj.optString("uvsn", null), jObj.optString("dvsn", null))
-      }
-
-      frontend.onChange(evt)
-    })
+    "changes" -> defineOnewayRpc((s:String) => s)(s => frontend.onChange(resolveEvent(deserializeEvent(s))))
   )
 }
