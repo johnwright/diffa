48e60afa90185cc9f0f5e72a3e83da3f556acc78
John Wright <john@lshift.net>
2011-01-19 15:00:18 +0000

    Moved AMQP connection checking code to amqp-messaging module, added tests for the AmqpQueueUrl class

---
+++ amqp-messaging/src/main/scala/net/lshift/diffa/messaging/amqp/AmqpConnectionChecker.scala
@@ -0,0 +1,11 @@
+package net.lshift.diffa.messaging.amqp
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: john
+ * Date: 19/01/11
+ * Time: 14:34
+ * To change this template use File | Settings | File Templates.
+ */
+
+object AmqpConnectionChecker
---
+++ amqp-messaging/src/test/scala/net/lshift/diffa/messaging/amqp/AmqpQueueUrlTests.scala
@@ -0,0 +1,11 @@
+package net.lshift.diffa.messaging.amqp
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: john
+ * Date: 19/01/11
+ * Time: 13:21
+ * To change this template use File | Settings | File Templates.
+ */
+
+class AmqpQueueUrlTests
