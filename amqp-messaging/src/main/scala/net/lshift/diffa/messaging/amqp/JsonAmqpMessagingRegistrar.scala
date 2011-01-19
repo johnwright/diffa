f770a3e59c63a309129002f61f36f658a7776b0d
John Wright <john@lshift.net>
2011-01-19 15:03:58 +0000

    Extracted timeout value for AMQP-RPC participant clients to properties file

--- amqp-messaging/src/main/scala/net/lshift/diffa/messaging/amqp/JsonAmqpMessagingRegistrar.scala
+++ amqp-messaging/src/main/scala/net/lshift/diffa/messaging/amqp/JsonAmqpMessagingRegistrar.scala
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2010-2011 LShift Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.lshift.diffa.messaging.amqp
 
 import net.lshift.diffa.kernel.frontend.Changes
@@ -5,16 +21,21 @@ import net.lshift.diffa.kernel.participants.ParticipantFactory
 import net.lshift.diffa.kernel.protocol.ProtocolMapper
 import net.lshift.diffa.messaging.json.ChangesHandler
 
+/**
+ * Utility class responsible for registering JSON over AMQP protocol support with necessary factories
+ * and request handlers.
+ */
 class JsonAmqpMessagingRegistrar(connectorHolder: ConnectorHolder,
                                  protocolMapper: ProtocolMapper,
                                  participantFactory: ParticipantFactory,
-                                 changes: Changes) {
+                                 changes: Changes,
+                                 timeoutMillis: Long) {
 
   // Register the outbound participant factory for JSON/AMQP
-  val factory = new JsonAmqpParticipantProtocolFactory(connectorHolder)
+  val factory = new JsonAmqpParticipantProtocolFactory(connectorHolder, timeoutMillis)
   participantFactory.registerFactory(factory)
 
   // Register a handler so requests made to the /changes endpoint on the agent with inbound content types of
   // application/json are decoded by our ChangesHandler.
-  //protocolMapper.registerHandler("changes", new ChangesHandler(changes))
+  protocolMapper.registerHandler("changes", new ChangesHandler(changes))
 }
