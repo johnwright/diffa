31656ff4a32d9c64abb3cf1d0109150419a4328f
Ben Hood <0x6e6562@gmail.com>
2011-01-21 15:07:17 +0000

    Updated license headers

--- participants/bin/file-pair.sh
+++ participants/bin/file-pair.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2010 LShift Ltd.
+# Copyright (C) 2010-2011 LShift Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
