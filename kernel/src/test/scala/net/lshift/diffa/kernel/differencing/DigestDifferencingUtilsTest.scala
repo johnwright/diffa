fdae9fc320910f2b65320f1fc9193ca8ac08c2ef
Ben Hood <0x6e6562@gmail.com>
2011-04-21 18:00:12 +0100

    [#210] This test was too coarse grained and did not address the root of the issue, but it was useful in tracking the issue down

--- kernel/src/test/scala/net/lshift/diffa/kernel/differencing/DigestDifferencingUtilsTest.scala
+++ kernel/src/test/scala/net/lshift/diffa/kernel/differencing/DigestDifferencingUtilsTest.scala
@@ -46,24 +46,6 @@ class DigestDifferencingUtilsTest {
   }
 
   @Test
-  def dateRangeShouldNotWiden = {
-    val dateConstraint = DateRangeConstraint("bizDate", new LocalDate(2011,04,01), new LocalDate(2011,04,29))
-    val bucketing = Map("bizDate" -> YearlyCategoryFunction)
-
-    val remote = ArrayBuffer(AggregateDigest(Buffer("2011"),"2abbcf10cf9ae11c53191eaf8b121104"))
-    val local = List(AggregateDigest(List("2011"),"ae15bc55642738b49b0bad51c420e33b"))
-    val actions = DigestDifferencingUtils.differenceAggregates(remote,local,bucketing, Seq(dateConstraint))
-
-    assertEquals(1, actions.length)
-
-    val queryAction = actions(0).asInstanceOf[AggregateQueryAction]
-
-    assertEquals(1, queryAction.constraints.length)
-    assertEquals(dateConstraint, queryAction.constraints(0))
-
-  }
-
-  @Test
   def shouldReportNothingOnMatchingEmptyLists {
     val actions = DigestDifferencingUtils.differenceAggregates(Seq(), Seq(), Map(), Seq(unconstrained))
     assertEquals(0, actions.length)
