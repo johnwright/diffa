f519292eac1db7b73a157f20ac8d4dcb6803bd2e
Tim Clark <tim@mwmbles.home>
2011-05-25 10:48:37 +0100

    Added clickable link to stop and start polling

--- agent/src/main/webapp/js/graph.js
+++ agent/src/main/webapp/js/graph.js
@@ -16,50 +16,50 @@ var gutterSize = 14;
 var rightLimit = 0;
 
 function initCanvas() {
-  display = document.getElementById("display");
+	display = document.getElementById("display");
 
-  canvas = document.getElementById("canvas");
-  context = canvas.getContext("2d");
-  overlay = document.getElementById("overlay");
-  overlayContext = overlay.getContext("2d");
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");
+	overlay = document.getElementById("overlay");
+	overlayContext = overlay.getContext("2d");
 
-  underlay = document.getElementById("underlay");
-  underlayContext = underlay.getContext("2d");
+	underlay = document.getElementById("underlay");
+	underlayContext = underlay.getContext("2d");
 
-  scale = document.getElementById("scale");
-  scaleContext = scale.getContext("2d");
+	scale = document.getElementById("scale");
+	scaleContext = scale.getContext("2d");
 
-  rightLimit = (maxColumns * gridSize) - canvas.width;
+	rightLimit = (maxColumns * gridSize) - canvas.width;
 }
 
 function clearCanvas() {
-  canvas.width = canvas.width;
+	canvas.width = canvas.width;
 }
 
 function clearOverlay() {
-  overlay.width = overlay.width;
+	overlay.width = overlay.width;
 }
 
 function clearUnderlay() {
-  underlay.width = underlay.width;
+	underlay.width = underlay.width;
 }
 
 function clearScale() {
-  scale.width = scale.width;
+	scale.width = scale.width;
 }
 
 var sessionId = null;
 
 function createSession(cb) {
-  var handleSessionId = function(data, status, req) {
-    var location = req.getResponseHeader('Location');
-    var parts = location.split("/");
-    var sessionID = parts[parts.length - 1];
-    sessionId = sessionID;
+	var handleSessionId = function(data, status, req) {
+		var location = req.getResponseHeader('Location');
+		var parts = location.split("/");
+		var sessionID = parts[parts.length - 1];
+		sessionId = sessionID;
 
-    cb();
-  };
-  $.post(API_BASE + '/diffs/sessions', {}, handleSessionId, "json");
+		cb();
+	};
+	$.post(API_BASE + '/diffs/sessions', {}, handleSessionId, "json");
 }
 
 var startTime, endTime;
@@ -67,277 +67,292 @@ var bucketSize = 3600;
 
 var swimlaneLabels = [];
 function loadTestData() {
-  buckets = [];
-  for (var i = 0; i < maxRows; i++) {
-    var row = [];
-    for (var j = 0; j < maxColumns; j++) {
-      row[j] = 0;
-    }
-    buckets[i] = row;
-  }
+	buckets = [];
+	for (var i = 0; i < maxRows; i++) {
+		var row = [];
+		for (var j = 0; j < maxColumns; j++) {
+			row[j] = 0;
+		}
+		buckets[i] = row;
+	}
 
-  endTime = new Date();
-  var now = endTime.formatString("YYYY0MM0DDT0hh0mm0ssZ");
+	endTime = new Date();
+	var now = endTime.formatString("YYYY0MM0DDT0hh0mm0ssZ");
 
-  startTime = new Date(endTime - (3600 * maxColumns * 1000));
-  var dayBeforeNow = startTime.formatString("YYYY0MM0DDT0hh0mm0ssZ");
+	startTime = new Date(endTime - (3600 * maxColumns * 1000));
+	var dayBeforeNow = startTime.formatString("YYYY0MM0DDT0hh0mm0ssZ");
 
-  $.get("rest/diffs/sessions/" + sessionId + "/zoom?range-start=" + dayBeforeNow + "&range-end=" + now + "&bucketing=3600", function(data) {
-    var indexer = 0;
+	$.get("rest/diffs/sessions/" + sessionId + "/zoom?range-start=" + dayBeforeNow + "&range-end=" + now + "&bucketing=3600", function(data) {
+		var indexer = 0;
 
-    for (var pair in data) {
-      swimlaneLabels[indexer] = pair;
-      for (var x = 0; x < data[pair].length; x++) {
-        buckets[indexer][x] = data[pair][x];
-      }
-      indexer++;
-    }
-    clearEverything();
-    o_x = -1 * rightLimit;
-    context.translate(o_x, o_y);
-    scaleContext.translate(o_x, o_y);
-    drawGrid();
-  });
+		for (var pair in data) {
+			swimlaneLabels[indexer] = pair;
+			for (var x = 0; x < data[pair].length; x++) {
+				buckets[indexer][x] = data[pair][x];
+			}
+			indexer++;
+		}
+		clearEverything();
+		o_x = -1 * rightLimit;
+		context.translate(o_x, o_y);
+		scaleContext.translate(o_x, o_y);
+		drawGrid();
+	});
 }
 
 var timeout;
 var polling = true;
 function startPolling() {
-  clearTimeout(timeout);
-  createSession(function() {
-    loadTestData();
-  });
-  timeout = window.setTimeout(startPolling, 5000);
+	polling = true;
+	clearTimeout(timeout);
+	createSession(function() {
+		loadTestData();
+	});
+	timeout = window.setTimeout(startPolling, 5000);
 }
 
 function stopPolling() {
-  clearTimeout(timeout);
+	polling = false;
+	clearTimeout(timeout);
 }
 
 function dashedLine(ctx, x1, y1, x2, y2, dashLen) {
-  if (dashLen == undefined) dashLen = 2;
+	if (dashLen == undefined) dashLen = 2;
 
-  ctx.beginPath();
-  ctx.moveTo(x1, y1);
+	ctx.beginPath();
+	ctx.moveTo(x1, y1);
 
-  var dX = x2 - x1;
-  var dY = y2 - y1;
-  var dashes = Math.floor(Math.sqrt(dX * dX + dY * dY) / dashLen);
-  var dashX = dX / dashes;
-  var dashY = dY / dashes;
+	var dX = x2 - x1;
+	var dY = y2 - y1;
+	var dashes = Math.floor(Math.sqrt(dX * dX + dY * dY) / dashLen);
+	var dashX = dX / dashes;
+	var dashY = dY / dashes;
 
-  var q = 0;
-  while (q++ < dashes) {
-    x1 += dashX;
-    y1 += dashY;
-    if (q % 2 == 0) {
-      ctx.moveTo(x1, y1);
-    }
-    else {
-      ctx.lineTo(x1, y1);
-    }
-  }
-  if (q % 2 == 0) {
-    ctx.moveTo(x1, y1);
-  }
-  else {
-    ctx.lineTo(x1, y1);
-  }
+	var q = 0;
+	while (q++ < dashes) {
+		x1 += dashX;
+		y1 += dashY;
+		if (q % 2 == 0) {
+			ctx.moveTo(x1, y1);
+		}
+		else {
+			ctx.lineTo(x1, y1);
+		}
+	}
+	if (q % 2 == 0) {
+		ctx.moveTo(x1, y1);
+	}
+	else {
+		ctx.lineTo(x1, y1);
+	}
 
-  ctx.stroke();
-  ctx.closePath();
+	ctx.stroke();
+	ctx.closePath();
 }
 
 var selected;
 function drawCircle(i, j) {
-  var cell = coordsToPosition({"x":i, "y":j});
-  if (cell.column < maxColumns && cell.row < maxRows) {
-    var cell_x = i + Math.floor(gridSize / 2);
-    var cell_y = j + gutterSize + Math.floor(gridSize / 2);
-    var size = limit(buckets[cell.row][cell.column], Math.floor((gridSize - 1) / 2));
-    if (size.limited) {
-      context.lineWidth = 2;
-    }
-    else {
-      context.lineWidth = 1;
-    }
-    context.strokeStyle = "black";
-    context.fillStyle = "white";
-    context.beginPath();
-    context.arc(cell_x, cell_y, size.value, 0, Math.PI * 2, false);
-    context.closePath();
-    context.stroke();
-    context.fill();
-  }
+	var cell = coordsToPosition({"x":i, "y":j});
+	if (cell.column < maxColumns && cell.row < maxRows) {
+		var cell_x = i + Math.floor(gridSize / 2);
+		var cell_y = j + gutterSize + Math.floor(gridSize / 2);
+		var size = limit(buckets[cell.row][cell.column], Math.floor((gridSize - 1) / 2));
+		if (size.limited) {
+			context.lineWidth = 2;
+		}
+		else {
+			context.lineWidth = 1;
+		}
+		context.strokeStyle = "black";
+		context.fillStyle = "white";
+		context.beginPath();
+		context.arc(cell_x, cell_y, size.value, 0, Math.PI * 2, false);
+		context.closePath();
+		context.stroke();
+		context.fill();
+	}
 }
 
 var show_grid = false;
 function drawGrid() {
-  var region_width = maxColumns * gridSize;
-  if (show_grid) {
-    for (var x = 0.5; x < region_width; x += gridSize) {
-      context.moveTo(x, 0);
-      context.lineTo(x, canvas.height);
-    }
-    for (var y = 0.5; y < canvas.height; y += (2 * gutterSize + gridSize)) {
-      context.moveTo(0, y);
-      context.lineTo(region_width, y);
-    }
-    context.strokeStyle = "red";
-    context.stroke();
-  }
+	var region_width = maxColumns * gridSize;
+	if (show_grid) {
+		for (var x = 0.5; x < region_width; x += gridSize) {
+			context.moveTo(x, 0);
+			context.lineTo(x, canvas.height);
+		}
+		for (var y = 0.5; y < canvas.height; y += (2 * gutterSize + gridSize)) {
+			context.moveTo(0, y);
+			context.lineTo(region_width, y);
+		}
+		context.strokeStyle = "red";
+		context.stroke();
+	}
 
-  var lane = 0;
-  for (var s = 0.5 + (2 * gutterSize + gridSize); s < canvas.height; s += (2 * gutterSize + gridSize)) {
-    dashedLine(underlayContext, 0, s, canvas.width, s, 2);
-    if (swimlaneLabels[lane] != null) {
-      underlayContext.font = "11px serif";
-      underlayContext.fillText(swimlaneLabels[lane], 10, s - 3);
-    }
-    lane++;
-  }
+	var lane = 0;
+	for (var s = 0.5 + (2 * gutterSize + gridSize); s < canvas.height; s += (2 * gutterSize + gridSize)) {
+		dashedLine(underlayContext, 0, s, canvas.width, s, 2);
+		if (swimlaneLabels[lane] != null) {
+			underlayContext.font = "11px serif";
+			underlayContext.fillText(swimlaneLabels[lane], 10, s - 3);
+		}
+		lane++;
+	}
 
 
-  for (var i = 0.5; i < region_width; i += gridSize) {
-    for (var j = 0.5; j < canvas.height; j += (2 * gutterSize + gridSize)) {
-      drawCircle(i, j);
-    }
-  }
+	for (var i = 0.5; i < region_width; i += gridSize) {
+		for (var j = 0.5; j < canvas.height; j += (2 * gutterSize + gridSize)) {
+			drawCircle(i, j);
+		}
+	}
 
-  scaleContext.font = "9px sans-serif";
-  for (var sc = 0; sc < maxColumns; sc++) {
-    if (sc % 3 == 0) {
-      var tick = new Date(startTime.getTime() + (sc * bucketSize * 1000));
-      scaleContext.fillText(tick.formatString("0DD/0MM"), sc * gridSize, 10);
-      scaleContext.fillText(tick.formatString("0hh:0mm"), sc * gridSize, 20);
-    }
-  }
+	scaleContext.font = "9px sans-serif";
+	for (var sc = 0; sc < maxColumns; sc++) {
+		if (sc % 3 == 0) {
+			var tick = new Date(startTime.getTime() + (sc * bucketSize * 1000));
+			scaleContext.fillText(tick.formatString("0DD/0MM"), sc * gridSize, 10);
+			scaleContext.fillText(tick.formatString("0hh:0mm"), sc * gridSize, 20);
+		}
+	}
 }
 
 var highlighted;
 function drawOverlay() {
-  if (highlighted != null && highlighted.column >= 0 && highlighted.row >= 0) {
-    var value = buckets[highlighted.row][highlighted.column];
-    if (value > 0) {
-      var c_x = highlighted.column * gridSize;
-      var c_y = (highlighted.row * (2 * gutterSize + gridSize)) + gutterSize;
-      overlayContext.font = "12px sans-serif";
-      overlayContext.textBaseline = "bottom";
-      var width = context.measureText("" + value).width;
-      overlayContext.fillText(value, c_x + Math.floor(gridSize / 2) - Math.floor(width / 2), c_y);
-    }
-  }
+	if (highlighted != null && highlighted.column >= 0 && highlighted.row >= 0) {
+		var value = buckets[highlighted.row][highlighted.column];
+		if (value > 0) {
+			var c_x = highlighted.column * gridSize;
+			var c_y = (highlighted.row * (2 * gutterSize + gridSize)) + gutterSize;
+			overlayContext.font = "12px sans-serif";
+			overlayContext.textBaseline = "bottom";
+			var width = context.measureText("" + value).width;
+			overlayContext.fillText(value, c_x + Math.floor(gridSize / 2) - Math.floor(width / 2), c_y);
+		}
+	}
 }
 
 function limit(value, maximum) {
-  if (value <= maximum) {
-    return {"value":value, "limited":false};
-  }
-  return {"value":maximum, "limited":true};
+	if (value <= maximum) {
+		return {"value":value, "limited":false};
+	}
+	return {"value":maximum, "limited":true};
 }
 
 var o_x = rightLimit;
 var o_y = 0;
 function coords(e) {
-  var x;
-  var y;
-  if (e.pageX != undefined && e.pageY != undefined) {
-    x = e.pageX;
-    y = e.pageY;
-  }
-  else {
-    x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-    y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-  }
+	var x;
+	var y;
+	if (e.pageX != undefined && e.pageY != undefined) {
+		x = e.pageX;
+		y = e.pageY;
+	}
+	else {
+		x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+		y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+	}
 
-  x -= display.offsetLeft;
-  y -= display.offsetTop;
+	x -= display.offsetLeft;
+	y -= display.offsetTop;
 
-  return { "x":x, "y":y };
+	return { "x":x, "y":y };
 }
 
 function coordsToPosition(coords) {
-  return {
-    "row": Math.floor(coords.y / (2 * gutterSize + gridSize)),
-    "column": Math.floor((coords.x) / gridSize)
-  };
+	return {
+		"row": Math.floor(coords.y / (2 * gutterSize + gridSize)),
+		"column": Math.floor((coords.x) / gridSize)
+	};
 }
 
 var dragging = false;
 function mouseDown(e) {
-  switch (e.which) {
-    case 3:
-      alert("RIGHT CLICK");
-      break;
-    default:
-      dragging = e;
-      stopPolling();
-      polling = false;
-      var c = coords(e);
-      c.x -= o_x;
-      selected = coordsToPosition(c);
-  }
+	switch (e.which) {
+		case 3:
+			alert("RIGHT CLICK");
+			break;
+		default:
+			dragging = e;
+			stopPolling();
+			polling = false;
+			var c = coords(e);
+			c.x -= o_x;
+			selected = coordsToPosition(c);
+	}
 }
 
 function mouseUp(e) {
-  dragging = false;
-  polling = true;
+	dragging = false;
+	polling = true;
 }
 
 function clearEverything() {
-  clearCanvas();
-  clearOverlay();
-  clearUnderlay();
-  clearScale();
+	clearCanvas();
+	clearOverlay();
+	clearUnderlay();
+	clearScale();
 }
 
 function mouseMove(e) {
-  if (dragging) {
-    clearEverything()
-    var m_coords = coords(e);
-    var d_coords = coords(dragging);
-    o_x += m_coords.x - d_coords.x;
-    if (o_x > 0) {
-      o_x = 0;
-      //alert("This application can only show 7 days worth of data.\n\nThis restriction could be removed if you raise a change request with the Diffa development team.")
-    }
+	if (dragging) {
+		clearEverything()
+		var m_coords = coords(e);
+		var d_coords = coords(dragging);
+		o_x += m_coords.x - d_coords.x;
+		if (o_x > 0) {
+			o_x = 0;
+			//alert("This application can only show 7 days worth of data.\n\nThis restriction could be removed if you raise a change request with the Diffa development team.")
+		}
 
-    if (Math.abs(o_x) > rightLimit) {
-      o_x = -1 * rightLimit;
-    }
-    context.translate(o_x, o_y);
-    scaleContext.translate(o_x, 0);
-    drawGrid();
-    dragging = e;
-    $("#debug").text("Offset " + o_x + " " + rightLimit);
-  }
-  else {
-    clearOverlay();
-    overlayContext.translate(o_x, o_y);
-    mouseOver(e);
-  }
+		if (Math.abs(o_x) > rightLimit) {
+			o_x = -1 * rightLimit;
+		}
+		context.translate(o_x, o_y);
+		scaleContext.translate(o_x, 0);
+		drawGrid();
+		dragging = e;
+		$("#debug").text("Offset " + o_x + " " + rightLimit);
+	}
+	else {
+		clearOverlay();
+		overlayContext.translate(o_x, o_y);
+		mouseOver(e);
+	}
 }
 
 function mouseOver(e) {
-  var c = coords(e);
-  c.x -= o_x;
-  var position = coordsToPosition(c);
-  if (position.row >= 0 && position.row < maxRows && position.column >= 0 && position.column < maxColumns) {
-    highlighted = position;
-    drawOverlay();
-  }
+	var c = coords(e);
+	c.x -= o_x;
+	var position = coordsToPosition(c);
+	if (position.row >= 0 && position.row < maxRows && position.column >= 0 && position.column < maxColumns) {
+		highlighted = position;
+		drawOverlay();
+	}
 }
 
 function initGraph() {
-  initCanvas();
-  startPolling();
+	initCanvas();
+	startPolling();
 
-  $(document).mouseup(mouseUp);
-  $("#display").mousedown(mouseDown);
-  $(document).mousemove(mouseMove);
+	$(document).mouseup(mouseUp);
+	$("#display").mousedown(mouseDown);
+	$(document).mousemove(mouseMove);
 
 
-  $("#display").bind("contextmenu", function(e) {
-    return false;
-  });
+	$("#display").bind("contextmenu", function(e) {
+		return false;
+	});
+
+	$("#polling").toggle(
+		function() {
+			if(polling)	{
+				stopPolling();
+				$("#polling").text("Start polling");
+			}
+		},
+		function() {
+			startPolling();
+			$("#polling").text("Start polling");
+		}
+	);
 }
