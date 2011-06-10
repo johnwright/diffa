955d4efc70df1bf62a01c7a78a012c25a2209467
Paul Jones <pauljones23@gmail.com>
2011-06-10 07:32:15 +0100

    [#261] Add JSON-aware assertion

--- participant-support/src/test/java/net/lshift/diffa/participant/scanning/JSONHelperTest.java
+++ participant-support/src/test/java/net/lshift/diffa/participant/scanning/JSONHelperTest.java
@@ -1,10 +1,13 @@
 package net.lshift.diffa.participant.scanning;
 
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -19,14 +22,14 @@ public class JSONHelperTest {
   @Test
   public void shouldSerialiseEmptyList() throws Exception {
     String emptyRes = serialiseResult(new ArrayList<ScanResultEntry>());
-    assertEquals("[]", emptyRes);
+    assertJSONEquals("[]", emptyRes);
   }
 
   @Test
   public void shouldSerialiseSingleEntityWithNoAttributes() throws Exception {
     String single = serialiseResult(Arrays.asList(
       ScanResultEntry.forEntity("id1", "v1", new DateTime(2011, 6, 5, 15, 3, 0, 0, DateTimeZone.UTC))));
-    assertEquals("[{\"id\":\"id1\",\"version\":\"v1\",\"lastUpdated\":\"2011-06-05T15:03:00.000Z\"}]", single);
+    assertJSONEquals("[{\"id\":\"id1\",\"version\":\"v1\",\"lastUpdated\":\"2011-06-05T15:03:00.000Z\"}]", single);
   }
 
   @Test
@@ -34,7 +37,7 @@ public class JSONHelperTest {
     String single = serialiseResult(Arrays.asList(
       ScanResultEntry.forEntity("id1", "v1", new DateTime(2011, 6, 5, 15, 3, 0, 0, DateTimeZone.UTC),
           generateAttributes("a1v1", "a2v2"))));
-    assertEquals(
+    assertJSONEquals(
       "[{\"id\":\"id1\",\"attributes\":{\"a1\":\"a1v1\",\"a2\":\"a2v2\"},\"version\":\"v1\",\"lastUpdated\":\"2011-06-05T15:03:00.000Z\"}]",
       single);
   }
@@ -51,5 +54,13 @@ public class JSONHelperTest {
     result.put("a1", a1);
     result.put("a2", a2);
     return result;
+  }
+
+  private static void assertJSONEquals(String expected, String actual) throws IOException {
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNode expectedTree = mapper.readTree(expected);
+    JsonNode actualTree = mapper.readTree(actual);
+
+    assertEquals(expectedTree, actualTree);
   }
 }
