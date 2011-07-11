b286fb0382f7c7ce12c3d0f4369a73782ddd9d85
Ben Hood <0x6e6562@gmail.com>
2011-07-11 12:50:35 +0100

    Add packages description

--- s3_upload.sh
+++ s3_upload.sh
@@ -2,6 +2,12 @@
 
 BUCKET=s3://diffa-packages
 
+sed s/DIFFA_BUILD_NUMBER/${BUILD_NUMBER}/ etc/packages.js.in > packages.js
+
 s3cmd rb --recursive $BUCKET
+
 s3cmd mb $BUCKET
+
 s3cmd put dist/target/*.zip $BUCKET
+s3cmd put agent/target/*.war $BUCKET
+s3cmd put packages.js $BUCKET
