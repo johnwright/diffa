18f19f24d7a0e9613c632843dc4b2536c7aa017f
Anthony Harris <anthony@mrnoisy>
2012-02-22 12:11:56 +0000

    Force disconnection of any sessions of the user we need to drop and recreate so that the drop user statement will always succeed.

--- kernel/src/test/scala/net/lshift/diffa/kernel/util/OracleSchemaCleaner.scala
+++ kernel/src/test/scala/net/lshift/diffa/kernel/util/OracleSchemaCleaner.scala
@@ -1,6 +1,8 @@
 package net.lshift.diffa.kernel.util
 
 import net.lshift.diffa.kernel.util.SessionHelper.sessionFactoryToSessionHelper
+import org.hibernate.jdbc.Work
+import java.sql.Connection
 
 /**
  * Implements the SchemaCleaner for Oracle databases.
@@ -9,8 +11,35 @@ object OracleSchemaCleaner extends SchemaCleaner {
   override def clean(sysUserEnvironment: DatabaseEnvironment, appEnvironment: DatabaseEnvironment) {
     val schemaName = appEnvironment.username
     val password = appEnvironment.password
-    val configuration = sysUserEnvironment.getHibernateConfiguration
-    val sessionFactory = configuration.buildSessionFactory
+    val dbaConfig = sysUserEnvironment.getHibernateConfiguration
+    val sessionFactory = dbaConfig.buildSessionFactory
+
+    sessionFactory.withSession(session => {
+      session.doWork(new Work {
+        def execute(connection: Connection) {
+          val getSessionInfo = "select sid, serial# from v$session where username = '%s'".format(schemaName.toUpperCase)
+          val sessionInfoStmt = connection.prepareStatement(getSessionInfo)
+          val rs = sessionInfoStmt.executeQuery
+          var sessions: List[(Int, Int)] = Nil
+          while (rs.next()) {
+            sessions = (rs.getInt("sid"), rs.getInt("serial#")) :: sessions
+          }
+
+          sessions foreach {
+            sessionInfoPair: Tuple2[Int, Int] =>
+              val (sid, serialnum) = sessionInfoPair
+              val disconnectUser = "alter system disconnect session '%d,%d' immediate".format(sid, serialnum)
+              val disconnectStmt = connection.prepareStatement(disconnectUser)
+              try {
+                disconnectStmt.execute
+              } catch {
+                case ex =>
+                  println("Failed to disconnect session [%d/%d]".format(sid, serialnum))
+              }
+          }
+        }
+      })
+    })
 
     val dropSchemaStatement = "drop user %s cascade".format(schemaName)
     val createSchemaStatement = "create user %s identified by %s".format(schemaName, password)
