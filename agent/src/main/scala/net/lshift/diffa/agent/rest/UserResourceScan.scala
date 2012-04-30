8b82400faf0ac927d8413a447001053ead1ead84
Ceri Storey <ceri@lshift.net>
2012-04-30 19:13:58 +0100

    Calculate aggregate versions correctly in UserResourceScan.

--- agent/src/main/scala/net/lshift/diffa/agent/rest/UserResourceScan.scala
+++ agent/src/main/scala/net/lshift/diffa/agent/rest/UserResourceScan.scala
@@ -2,22 +2,22 @@ package net.lshift.diffa.agent.rest
 
 import net.lshift.diffa.kernel.config.system.SystemConfigStore
 import net.lshift.diffa.kernel.participants.CategoryFunction
-import net.lshift.diffa.participant.scanning.{ScanResultEntry, ScanConstraint}
 import net.lshift.diffa.kernel.config.User
 import scala.collection.JavaConversions._
+import net.lshift.diffa.kernel.frontend.SystemConfiguration
+import net.lshift.diffa.participant.scanning.{ScanAggregation, ScanResultEntry, ScanConstraint}
 
 
-class UserResourceScan (systemConfigStore: SystemConfigStore)  {
+class UserResourceScan (systemConfig: SystemConfiguration)  {
   // def perform() = {};
 
-  def perform(constraints:Seq[ScanConstraint], aggregation:Seq[CategoryFunction]): Seq[ScanResultEntry] = {
+  def perform(constraints:Seq[ScanConstraint], aggregation:Seq[ScanAggregation]): Seq[ScanResultEntry] = {
     def generateVersion(user:User) = ScannableUtils.generateDigest(user.name, user.token)
-         var users = systemConfigStore.listUsers
-
-         var scanResults = users.map  { u =>
-           ScanResultEntry.forEntity(u.name, generateVersion(u), null, Map("name" -> u.name))
-         }
-         ScannableUtils.maybeAggregate(scanResults, aggregation)
+    val users = ScannableUtils.filterByKey[User](systemConfig.listFullUsers, constraints, _.name)
+    val scanResults = users.map  { u =>
+      ScanResultEntry.forEntity(u.name, generateVersion(u), null, Map("name" -> u.name))
+    }
+    ScannableUtils.maybeAggregate(scanResults, aggregation)
   }
 
 }
--- agent/src/test/scala/net/lshift/diffa/agent/rest/UserResourceScanTest.scala
+++ agent/src/test/scala/net/lshift/diffa/agent/rest/UserResourceScanTest.scala
@@ -7,25 +7,32 @@ import scala.collection.JavaConversions._
 import org.junit.experimental.theories.{Theories, DataPoint,Theory}
 import org.junit.runner.RunWith
 import org.junit.Assert._
-import org.easymock.EasyMock._
+import org.easymock.EasyMock.{expect}
+import org.easymock.classextension.EasyMock.{replay,createStrictMock,verify}
 import net.lshift.diffa.kernel.config.system.SystemConfigStore
 import net.lshift.diffa.agent.rest.UserResourceScanTest.Scenario
 import net.lshift.diffa.kernel.config.User
-import net.lshift.diffa.participant.scanning.{ScanConstraint, ScanResultEntry}
 import org.junit.{Before, Test}
 import net.lshift.diffa.kernel.participants.{StringPrefixCategoryFunction, CategoryFunction}
+import net.lshift.diffa.participant.scanning.{StringPrefixConstraint, ScanConstraint, ScanResultEntry}
+import net.lshift.diffa.kernel.frontend.{UserDef, SystemConfiguration}
+import java.security.MessageDigest
+import net.lshift.diffa.agent.rest.ScannableUtils
+import scala.Predef._
+import org.apache.commons.codec.binary.Hex
+import java.nio.charset.Charset
 
 
 @RunWith(classOf[Theories])
 class UserResourceScanTest {
-  val systemConfigStore = createStrictMock("systemConfigStore", classOf[SystemConfigStore])
+  val systemConfig = createStrictMock("systemConfiguration", classOf[SystemConfiguration])
 
-  var userResourceScan = new UserResourceScan(systemConfigStore)
+  var userResourceScan = new UserResourceScan(systemConfig)
 
 
   def setup(scenario: Scenario) {
-    expect(systemConfigStore.listUsers).andReturn(scenario.userList).once()
-    replay(systemConfigStore)
+    expect(systemConfig.listFullUsers).andReturn(scenario.userList).once()
+    replay(systemConfig)
   }
 
   @Theory
@@ -34,7 +41,7 @@ class UserResourceScanTest {
 
     userResourceScan.perform(scenario.constraints, scenario.aggregation)
 
-    verify(systemConfigStore)
+    verify(systemConfig)
     //throw new RuntimeException("Kaboom!")
   }
 
@@ -55,22 +62,61 @@ object UserResourceScanTest {
 
 
   @DataPoint def nullExample = Scenario(List(), List(), List(), List())
-  def a_user = User("abc")
-  def b_user = User("bcd")
+  val user_abc = User("abc")
+  val user_abd = User("abd")
+  val user_bcd = User("bcd")
 
   @DataPoint def singleUserNoAggregation = Scenario(
-    List(a_user),
+    List(user_abc),
     List(),// Constraints
     List(), // Aggregation.
-    List(ScanResultEntry.forEntity(a_user.name, ScannableUtils.generateDigest(a_user.name, a_user.token), null, Map("name" -> "abc")))
+    List(ScanResultEntry.forEntity(user_abc.name,
+      ScannableUtils.generateDigest(user_abc.name, user_abc.token),
+      null, Map("name" -> user_abc.name)))
   )
 
-  @DataPoint def singleUserWithPrefixAggregation = {
-    var agg = ScanResultEntry.forAggregate("ec0405c5aef93e771cd80e0db180b88b", Map("name" -> "a"))
+  @DataPoint def twoUsersWithPrefixFilter = Scenario(
+    List(user_abc, user_bcd),
+    List(new StringPrefixConstraint("name", "b")),
+    List(), // Aggregation.
+    List(ScanResultEntry.forEntity(user_bcd.name,
+      ScannableUtils.generateDigest(user_bcd.name, user_bcd.token),
+      null, Map("name" -> user_bcd.name)))
+  )
+
+  def aggregated_versions(vsns: Seq[String]) = {
+
+    val md = MessageDigest.getInstance("MD5")
+    vsns.foreach { s => md.update(s.getBytes(Charset.forName("UTF-8"))) }
+    new String (Hex.encodeHex(md.digest()))
+  }
+
+  @DataPoint def twoUsersWithPrefixFilterAndAggregation = {
+    var matching_users = List(user_abc, user_abd)
+    val versions: List[String] = matching_users.map { user =>
+      ScannableUtils.generateDigest(user.name, user.token)
+    }
+    var agg_version = aggregated_versions(versions)
     Scenario(
-      List(a_user),
+      List(user_abc, user_abd, user_bcd),
+      List(new StringPrefixConstraint("name", "a")),
+      List(StringPrefixCategoryFunction("name", 1, 3, 1)),
+      List(ScanResultEntry.forAggregate(
+      agg_version,
+      Map("name" -> "a")))
+  )
+  }
+
+
+  @DataPoint def singleUserWithPrefixAggregation = {
+    val agg = ScanResultEntry.forAggregate("ec0405c5aef93e771cd80e0db180b88b", Map("name" -> "a"))
+    Scenario(
+      List(user_abc),
       List(),// Constraints
       List(StringPrefixCategoryFunction("name", 1, 3, 1)),
       List(agg))
   }
+
+   // Result ordering?
+
 }
