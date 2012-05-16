7c58eb8b238047460adbfa9755bae5e61bf524c0
Ben Hood <0x6e6562@gmail.com>
2012-05-16 14:40:32 +0100

    Added test to verify deleting a subset of a cache

--- kernel/src/test/scala/net/lshift/diffa/kernel/util/cache/CacheProviderTest.scala
+++ kernel/src/test/scala/net/lshift/diffa/kernel/util/cache/CacheProviderTest.scala
@@ -3,6 +3,7 @@ package net.lshift.diffa.kernel.util.cache
 import org.junit.Assert._
 import org.junit.runner.RunWith
 import org.junit.experimental.theories.{DataPoint, Theories, Theory}
+import reflect.BeanProperty
 
 @RunWith(classOf[Theories])
 class CacheProviderTest {
@@ -11,8 +12,7 @@ class CacheProviderTest {
   def shouldProvideReadThroughCaching(provider:CacheProvider) {
     val cache = provider.getCachedMap[String,String]("some-cache")
 
-    cache.evictAll
-    assertEquals(0, cache.size)
+    reset(cache)
 
     val underlying = new UnderlyingDataSource("some-value")
     val unCachedResponse = cache.readThrough("some_key", underlying.getData)
@@ -22,10 +22,48 @@ class CacheProviderTest {
     assertEquals(1, underlying.invocations)
   }
 
+  @Theory
+  def shouldSupportRemovalBasedOnKeys(provider:CacheProvider) {
+    val cache = provider.getCachedMap[TestCacheKey,String]("another-cache")
+
+    reset(cache)
+
+    cache.put(TestCacheKey("foo", 1), "first-value")
+    cache.put(TestCacheKey("bar", 2), "second-value")
+
+    assertEquals(2, cache.size())
+    assertEquals("first-value", cache.get(TestCacheKey("foo", 1)))
+    assertEquals("second-value", cache.get(TestCacheKey("bar", 2)))
+
+    cache.subset(TestKeyPredicate("foo")).evictAll
+
+    assertEquals(1, cache.size())
+    assertNull(cache.get(TestCacheKey("foo", 1)))
+    assertEquals("second-value", cache.get(TestCacheKey("bar", 2)))
+
+  }
+
+  private def reset(cache:CachedMap[_,_]) = {
+    cache.evictAll
+    assertEquals(0, cache.size)
+  }
+
 }
 
 object CacheProviderTest {
   @DataPoint def hazelcast = new HazelcastCacheProvider
+}
+
+case class TestCacheKey(
+  @BeanProperty var firstAttribute: String = null,
+  @BeanProperty var secondAttribute: java.lang.Integer = null) {
+
+  def this() = this(firstAttribute = null)
+}
+
+case class TestKeyPredicate(@BeanProperty firstAttribute:String) extends KeyPredicate[TestCacheKey] {
+  def this() = this(firstAttribute = null)
+  def constrain(key: TestCacheKey) = key.firstAttribute == firstAttribute
 }
 
 class UnderlyingDataSource(responseValue:String) {
