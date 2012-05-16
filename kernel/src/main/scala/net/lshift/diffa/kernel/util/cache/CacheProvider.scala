bf7504e73b94143c21ac9250f1931d1901cda403
Ben Hood <0x6e6562@gmail.com>
2012-05-16 16:42:04 +0100

    Docuemented the CacheProvider interface somewhat

--- kernel/src/main/scala/net/lshift/diffa/kernel/util/cache/CacheProvider.scala
+++ kernel/src/main/scala/net/lshift/diffa/kernel/util/cache/CacheProvider.scala
@@ -18,23 +18,54 @@ package net.lshift.diffa.kernel.util.cache
 import scala.collection.JavaConversions._
 import java.io.Serializable
 
+/**
+ * Service definition that abstracts away concrete cache implementations from higher level application code.
+ */
 trait CacheProvider {
 
+  /**
+   * Returns a reference to a managed cache instance identified by the given name.
+   */
   def getCachedMap[K,V](name:String) : CachedMap[K,V]
 
 }
 
-//case class CacheConfig(ttl:Int)
+/**
+ * This is an in-memory key-value store with a minimal set of primitives to facilitate caching.
+ */
+trait CachedMap[K,V] extends CachedSubset[K,V] {
 
-trait CachedMap[K,V] {
-
+  /**
+   * Returns the amount of entries in the cache.
+   */
   def size() : Int
+
+  /**
+   * Retrieves a value based on the supplied key from the cache.
+   */
   def get(key:K) : V
+
+  /**
+   * Inserts a value based on the supplied key into the cache.
+   */
   def put(key:K, value:V)
+
+  /**
+   * Removes a value based on the supplied key from the cache.
+   */
   def evict(key:K)
-  def evictAll
+
+  /**
+   * Retrieves a subset of the cache based on the supplied predicate.
+   */
   def subset(predicate:KeyPredicate[K]) : CachedSubset[K,V]
 
+  /**
+   * Retrieves a value based on the supplied key from the cache, if the value is already cached.
+   * If the value is not already cached, the supplied function is executed to retrieve the data
+   * from the underlying storage and returns this to the caller, inserting it into the cache against
+   * the given key.
+   */
   def readThrough(key:K, f:() => V ) : V = {
     val cached = get(key)
     if (null == cached) {
@@ -48,10 +79,25 @@ trait CachedMap[K,V] {
   }
 }
 
+/**
+ * This is used to reference a subset of a cache, e.g. to be able to invoke operations on a portion
+ * of the total values of a parent cache.
+ */
 trait CachedSubset[K,V] {
-  def evictAll
+
+  /**
+   *  Removes all the values from the referenced cache.
+   */
+  def evictAll()
 }
 
+/**
+ * Provides a way to query a cache based on cache keys (as opposed to cache values).
+ */
 trait KeyPredicate[K] extends Serializable {
+
+  /**
+   * Returns true if the current cache key should be included in the query results.
+   */
   def constrain(key:K) : Boolean
 }
