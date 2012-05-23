e956c0564c57c637da2912724872a88dcad03c0f
Ben Hood <0x6e6562@gmail.com>
2012-05-23 13:40:59 +0100

    Added somem documentation

--- kernel/src/main/scala/net/lshift/diffa/kernel/util/db/DatabaseFacade.scala
+++ kernel/src/main/scala/net/lshift/diffa/kernel/util/db/DatabaseFacade.scala
@@ -57,25 +57,71 @@ trait DatabaseFacade {
    */
   @Deprecated def insert[T](o:T) : T
 
-  def beginTransaction : Transaction
+  /**
+   * This is a (hopefully) portable wrapper around an underlying transactional resource.
+   * This exposes a handle to application code that allows several DB operations to be grouped into one DB transaction.
+   * This is marked as deprecated because architecturely speaking, we should ideally refactor the access to
+   * the DB to avoid having to group calls.
+   */
+  @Deprecated def beginTransaction : Transaction
 }
 
+/**
+ * Encapsulates the name of a DB query and the arguments used in its execution.
+ */
 case class DatabaseCommand(
   queryName:String,
   params:Map[String, Any]
 )
 
-trait Transaction {
+/**
+ * This provides a handle that application code can use to group DB calls into one logical unit of work.
+ *
+ * Usage:
+ *
+ * val tx = facade.beginTransaction
+ *
+ * ...
+ *
+ * tx.execute(command1)
+ * tx.execute(command2)
+ *
+ * ...
+ *
+ * tx.commit
+ *
+ */
+@Deprecated trait Transaction {
 
-  def singleQueryMaybe[T](command:DatabaseCommand) : Option[T]
+  /**
+   * Run a command against the DB and return the row count.
+   */
   def execute(command:DatabaseCommand) : Int
+
+  /**
+   * This is a thin wrapper around the (deprecated) Hibernate Session save/1 call.
+   */
   @Deprecated def insert[T](o:T) : T
+
+  /**
+   * Issue the commit command to the database, flushing out all previously submitting commands.
+   */
   def commit()
+
+  /**
+   * Registers a handler that will be invoked in the event that the transaction is rolled back.
+   * This allows application code to perform any necessary cleanup.
+   */
   def registerRollbackHandler(h:RollbackHandler)
 
 }
 
+@Deprecated
 trait RollbackHandler {
+
+  /**
+   * This event is fired when an owning transaction is rolled back.
+   */
   def onRollback()
 }
 
