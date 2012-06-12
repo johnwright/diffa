358d22a33ab51f3732adc1f036f134bd2e1679f6
John Wright <john@lshift.net>
2012-06-12 18:07:24 +0100

    Added a migration step for scan cron disabling/re-enabling support

---
+++ schema/src/main/scala/net/lshift/diffa/schema/migrations/steps/Step0033.scala
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) 2010-2012 LShift Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.lshift.diffa.schema.migrations.steps
+
+import java.sql.Types
+import org.hibernate.cfg.Configuration
+import net.lshift.hibernate.migrations.MigrationBuilder
+import net.lshift.diffa.schema.migrations.HibernateMigrationStep
+
+object Step0033 extends HibernateMigrationStep {
+
+  def versionId = 33
+
+  def name = "Add columns to support enabling/disabling of scan cron specs"
+
+  def createMigration(config: Configuration) = {
+    val migration = new MigrationBuilder(config)
+
+    migration
+      .alterTable("pair")
+      .addColumn("scan_cron_enabled", Types.BIT, 1, false, 1)
+
+    migration
+      .alterTable("pair_views")
+      .addColumn("scan_cron_enabled", Types.BIT, 1, false, 1)
+
+    migration
+  }
+}
