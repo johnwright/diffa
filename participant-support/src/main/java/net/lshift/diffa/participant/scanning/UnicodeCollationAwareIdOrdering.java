0de7ba61b57c0ff62bf3a441ea534f357dd94b45
Ceri Storey <ceri@lshift.net>
2012-06-13 14:04:49 +0100

    Update copyright notices.

--- participant-support/src/main/java/net/lshift/diffa/participant/scanning/UnicodeCollationAwareIdOrdering.java
+++ participant-support/src/main/java/net/lshift/diffa/participant/scanning/UnicodeCollationAwareIdOrdering.java
@@ -1,15 +1,23 @@
+/**
+ * Copyright (C) 2010-2011 LShift Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.lshift.diffa.participant.scanning;
 
 import com.ibm.icu.text.Collator;
 import java.util.Locale;
 
-/**
- * Created with IntelliJ IDEA.
- * User: ceri
- * Date: 12/06/12
- * Time: 19:07
- * To change this template use File | Settings | File Templates.
- */
 public class UnicodeCollationAwareIdOrdering implements IdOrdering {
   private Collator coll = Collator.getInstance(Locale.ROOT);
 
